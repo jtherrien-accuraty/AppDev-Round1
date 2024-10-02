b9efc7e8e4786707f6e3eeea36c42e683ffb2506
jtherrien-accuraty <jtherrien@accuraty.com>
2024-10-01 19:18:42 -0500

    Form behavior based on user interaction tested and verified

--- AppDev-Round1/AppDev-Round1/Form1.cs
+++ AppDev-Round1/AppDev-Round1/Form1.cs
@@ -4,6 +4,7 @@ namespace AppDev_Round1
 {
     public partial class Form1 : Form
     {
+        private string fileContents = string.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -11,7 +12,40 @@ namespace AppDev_Round1
 
         private void openFile_Click(object sender, EventArgs e)
         {
-            
+            if (openJSONFile.ShowDialog() == DialogResult.OK)
+            {
+                initializeForm();
+            }
+        }
+        /// <summary>
+        /// Resets form and processes selected JSON file
+        /// </summary>
+        private void initializeForm()
+        {
+
+            trainings.Items.Clear();
+            trainings.Enabled = false;
+            jsonPreview.Text = string.Empty;
+            outputPreview.Text = string.Empty;
+            outputType.Text = string.Empty;
+            outputType.Enabled = true;
+            outputTypeLbl.Enabled = true;
+            updateOutput.Enabled = false;
+            saveJSON.Enabled = false;
+            expiredTraining.Enabled = false;
+            expiredTrainingLbl.Enabled = false;
+            fiscalYear.Enabled = false;
+            fiscalYearLbl.Enabled = false;
+            processJson();
+        }
+
+
+        /// <summary>
+        /// Deserializes select JSON file and sets JSON File Preview to file contents
+        /// </summary>
+        private void processJson()
+        {
+            jsonPreview.Text += "Process JSON Function Called\r\n";
         }
 
         private void openJSONFile_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
@@ -21,7 +55,18 @@ namespace AppDev_Round1
 
         private void saveJSON_Click(object sender, EventArgs e)
         {
+            if (saveJSONFile.ShowDialog() == DialogResult.OK)
+            {
+                SaveOutput();
+            }
+        }
 
+        /// <summary>
+        /// Saves the generated output to .Json file
+        /// </summary>
+        private void SaveOutput()
+        {
+            outputPreview.Text = "Save Output Clicked";
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -31,7 +76,85 @@ namespace AppDev_Round1
 
         private void outputType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            updateOutput.Enabled = true;
+            switch (outputType.SelectedItem)
+            {
+                case "Completed Trainings":
+                    
+                    CompletedTrainingsSelected();
+                    break;
+                case "Training Reports":
+                    TrainingReportsSelected();
+                    break;
+                case "Expired Trainings":
+                    ExpiredTrainingsSelected();
+                    break;
+                default:
+                    updateOutput.Enabled = false;
+                    break;
+            }
+
+        }
+
+        private void CompletedTrainingsSelected()
+        {
+            trainings.Enabled = false;
+            trainingsLbl.Enabled = false;
+            fiscalYear.Enabled = false;
+            fiscalYearLbl.Enabled = false;
+            expiredTraining.Enabled = false;
+            expiredTrainingLbl.Enabled = false;
+        }
+
+        private void TrainingReportsSelected()
+        {
+            trainings.Enabled = true;
+            trainingsLbl.Enabled = true;
+            fiscalYear.Enabled = true;
+            fiscalYearLbl.Enabled = true;
+            expiredTraining.Enabled = false;
+            expiredTrainingLbl.Enabled = false;
+        }
+
+        private void ExpiredTrainingsSelected()
+        {
+            trainings.Enabled = false;
+            trainingsLbl.Enabled = false;
+            fiscalYear.Enabled = false;
+            fiscalYearLbl.Enabled = false;
+            expiredTraining.Enabled = true;
+            expiredTrainingLbl.Enabled = true;
+        }
+
+        private void ProcessCompletedTrainings()
+        {
+
+            outputPreview.Text = "Process Completed Trainings Selected";
+        }
+
+        private void ProcessTrainingReports()
+        {
+            outputPreview.Text = "Process Training Reports Selected";
+        }
+
+        private void updateOutput_Click(object sender, EventArgs e)
+        {
+            switch (outputType.SelectedItem)
+            {
+                case "Completed Trainings":
+                    ProcessCompletedTrainings();
+                    break;
+                case "Training Reports":
+                    ProcessTrainingReports();
+                    break;
+                case "Expired Trainings":
+                    ExpiredTrainingsSelected();
+                    break;
+                default:
+                    updateOutput.Enabled = false;
+                    return;
+            }
+            saveJSON.Enabled = true;
         }
     }
 }
